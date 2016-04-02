4451f7ba51848402795bfcceb8688e26c20cb472
TruongTuyen <hieuluong100495@gmail.com>
2016-04-02 09:32:20 +0700

    Initial

---
+++ classes/class.TT_Nhanvien.php
@@ -0,0 +1,12 @@
+<?php
+class TT_Nhanvien{
+    //Xu ly page hien thi danh sach nhan vien
+    public function tt_page_nhanvien_callback(){
+        
+    }
+    
+    //Xu ly page hien thi form them moi va cap nhap thong tin nhan vien
+    public function tt_new_nhanvien_callback(){
+        
+    }
+}
---
+++ thuctap.php
@@ -0,0 +1,249 @@
+<?php
+/*
+Plugin Name: WordPress Custom Table And WP_List_Table Example
+Description: A wordpress work with custom table in wordpress database and WP_List_Table to display data from database
+Plugin URI: https:localhost
+Author URI: https:localhost
+Author: Truong Tuyen Anh
+License: Public Domain
+Version: 1.0
+Text Domain: simple_plugin
+*/
+define( TT_DIR_PATH, plugin_dir_path( __FILE__ ) ); //Lấy ra dường dẫn tuyệt đối tới thu muc của plugin này 
+define( TT_DIR_URL, plugin_dir_url( __FILE__ ) ); //Lấy ra url của plugin này
+
+require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );//sử dụng file này để có thể dùng hàm dbDelta();
+require_once TT_DIR_PATH . 'classes/class.TT_KyNang.php'; 
+require_once TT_DIR_PATH . 'classes/class.TT_Nhanvien.php';
+
+if (!class_exists('WP_List_Table')) {
+    require_once(ABSPATH . 'wp-admin/includes/class-wp-list-table.php');
+}
+
+class TT_Teamwork{
+    public $db_version = '1.0';
+    
+    function __construct(){
+        register_activation_hook( __FILE__,  array( $this, 'create_table' ) );
+        register_activation_hook( __FILE__,  array( $this, 'dummy_data' ) );
+        register_deactivation_hook( __FILE__, array( $this, 'delete_table' ) );
+        
+        add_action( 'admin_menu', array( $this, 'register_setting_menu' ) );
+        add_action( 'init', array( $this, 'tt_load_languages' ) );
+    }
+    
+    public function create_table(){
+        global $wpdb;
+        $query = "
+            CREATE TABLE {$wpdb->prefix}duan(
+                id_duan BIGINT NOT NULL AUTO_INCREMENT,
+                tenduan VARCHAR(225) NOT NULL,
+                thoigianbatdau DATETIME NOT NULL,
+                thoigianketthuc DATETIME NOT NULL,
+                ghichu TEXT NULL,
+                PRIMARY KEY (id_duan)   
+            );
+                         
+            CREATE TABLE {$wpdb->prefix}nhanvien( 
+                id_nhanvien BIGINT NOT NULL AUTO_INCREMENT , 
+                hoten VARCHAR(225) NOT NULL , 
+                namsinh DATETIME NOT NULL , 
+                gioitinh TINYINT(2) NOT NULL ,
+                quequan VARCHAR(225) NOT NULL , 
+                PRIMARY KEY (id_nhanvien)
+            );
+            
+            CREATE TABLE {$wpdb->prefix}kynang(
+                id_kynang BIGINT NOT NULL AUTO_INCREMENT,
+                tenkynang VARCHAR(225) NOT NULL,
+                chuthich TEXT NULL,
+                PRIMARY KEY (id_kynang)
+            );
+            
+            CREATE TABLE {$wpdb->prefix}chitiet_duan(
+                id BIGINT NOT NULL AUTO_INCREMENT,
+                id_duan BIGINT NOT NULL,
+                id_nhanvien BIGINT NOT NULL,
+                ghichu TEXT NULL,
+                PRIMARY KEY (id)
+            );
+            
+            CREATE TABLE {$wpdb->prefix}chitiet_kynang(
+                id BIGINT NOT NULL AUTO_INCREMENT,
+                id_kynang BIGINT NOT NULL,
+                id_nhanvien BIGINT NOT NULL,
+                ghichu TEXT NULL,
+                PRIMARY KEY (id)
+            ); 
+        ";
+        dbDelta( $query );
+    }
+    
+    public function db_version_option( $version = '1.0' ){
+        add_option( 'db_version', $version );
+    }
+    
+    public function del_db_version(){
+        delete_option( 'db_version' );
+    }
+    
+    public function update_db_version( $new_version ){
+        update_option( 'db_version', $new_version );
+    }
+    
+    public function delete_table(){
+        global $wpdb;
+        $sql = "DROP TABLE IF EXISTS `{$wpdb->prefix}duan`, `{$wpdb->prefix}nhanvien`, `{$wpdb->prefix}kynang`, `{$wpdb->prefix}chitiet_duan`, `{$wpdb->prefix}chitiet_kynang`";
+        $wpdb->query( $sql );
+        $this->del_db_version();
+    }
+    
+    public function check_db_version(){
+        $current_version = get_option( 'db_version' );
+        if( $this->db_version != $current_version ){
+            $this->delete_table();
+            $this->create_table();
+            $this->update_db_version( $this->db_version );
+        }
+    }
+    
+    public function dummy_data(){
+        global $wpdb;
+        $wpdb->insert( $wpdb->prefix . 'duan', array(
+            'id_duan'            => 1,
+            'tenduan'            => 'Website Ban Hang 01',
+            'thoigianbatdau'     => '2015-10-16',
+            'thoigianketthuc'    => '2015-11-16',
+            'ghichu'             => null   
+        ));
+        $wpdb->insert( $wpdb->prefix . 'duan', array(
+            'id_duan'            => 2,
+            'tenduan'            => 'Website Ban Hang 02',
+            'thoigianbatdau'     => '2015-11-10',
+            'thoigianketthuc'    => '2015-12-10',
+            'ghichu'             => 'ghi chu 01'   
+        ));
+        
+        //du lieu mau cho bang _nhanvien
+        $wpdb->insert( $wpdb->prefix . 'nhanvien', array(
+            'id_nhanvien'       => 1, 
+            'hoten'             => 'Nguyen Van A', 
+            'namsinh'           => '1990-01-01', 
+            'gioitinh'          => 1,//quy uoc: gia tri: 1 <=> Nam, 2 <=> Nu,
+            'quequan'           => 'Thai Nguyen', 
+        ));
+        $wpdb->insert( $wpdb->prefix . 'nhanvien', array(
+            'id_nhanvien'       => 2, 
+            'hoten'             => 'Nguyen Van B', 
+            'namsinh'           => '1991-01-01', 
+            'gioitinh'          => 1,//quy uoc: gia tri: 1 <=> Nam, 2 <=> Nu,
+            'quequan'           => 'Thai Binh', 
+        ));
+        
+        //du lieu mau cho bang _kynang
+        $wpdb->insert( $wpdb->prefix . 'kynang', array(
+            'id_kynang'         => 1,
+            'tenkynang'         => 'HTML',
+            'chuthich'          => 'Ghi chu 01',
+        ));
+        $wpdb->insert( $wpdb->prefix . 'kynang', array(
+            'id_kynang'         => 2,
+            'tenkynang'         => 'CSS',
+            'chuthich'          => 'Ghi chu 02',
+        ));
+        $wpdb->insert( $wpdb->prefix . 'kynang', array(
+            'id_kynang'         => 3,
+            'tenkynang'         => 'PHP',
+            'chuthich'          => 'Ghi chu 03',
+        ));
+        
+        //du lieu mau cho bang: _chitiet_duan
+        $wpdb->insert( $wpdb->prefix . 'chitiet_duan', array(
+            'id'                => 1,
+        	'id_duan'           => 1,
+        	'id_nhanvien'       => 1,
+        	'ghichu'            => 'Ghi chu 01',
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_duan', array(
+            'id'                => 2,
+        	'id_duan'           => 1,
+        	'id_nhanvien'       => 2,
+        	'ghichu'            => 'Ghi chu 02',
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_duan', array(
+            'id'                => 3,
+        	'id_duan'           => 2,
+        	'id_nhanvien'       => 1,
+        	'ghichu'            => 'Ghi chu 01',
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_duan', array(
+            'id'                => 4,
+        	'id_duan'           => 2,
+        	'id_nhanvien'       => 2,
+        	'ghichu'            => 'Ghi chu 02',
+        ));
+        
+        //du lieu cho bang _chitiet_kynang
+        $wpdb->insert( $wpdb->prefix . 'chitiet_kynang', array(
+            'id'                => 1,
+    	    'id_kynang'         => 1,
+    	    'id_nhanvien'       => 1,
+        	'ghichu'            => "Ghi chu 01"
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_kynang', array(
+            'id'                => 2,
+    	    'id_kynang'         => 2,
+    	    'id_nhanvien'       => 1,
+        	'ghichu'            => "Ghi chu 02"
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_kynang', array(
+            'id'                => 3,
+    	    'id_kynang'         => 1,
+    	    'id_nhanvien'       => 2,
+        	'ghichu'            => "Ghi chu 03"
+        ));
+        $wpdb->insert( $wpdb->prefix . 'chitiet_kynang', array(
+            'id'                => 4,
+    	    'id_kynang'         => 2,
+    	    'id_nhanvien'       => 2,
+        	'ghichu'            => "Ghi chu 04"
+        ));
+    }  
+    
+    public function tt_teamwork_callback(){
+        
+    }
+    
+    public function register_setting_menu(){
+        add_menu_page( __("TT Teamwork", "simple_plugin"), __("TT Teamwork","simple_plugin"), "activate_plugins", "tt_teamwork", array( $this, "tt_teamwork_callback" ) );
+        
+        add_submenu_page( "tt_teamwork", __( "Danh sách nhân viên","simple_plugin" ), __( "Danh sách nhân viên","simple_plugin" ), "activate_plugins", "ds_nhanvien", array( "TT_Nhanvien", "tt_page_nhanvien_callback" ) );
+        add_submenu_page( "tt_teamwork", __( "Thêm mới nhân viên", "simple_plugin" ), __( "Thêm mới nhân viên", "simple_plugin" ), "activate_plugins", "new_nhanvien", array( "TT_Nhanvien", "tt_new_nhanvien_callback" ) );
+        
+        add_submenu_page( 'tt_teamwork', __( "Danh sách kỹ năng", "simple_plugin" ), __( "Danh sách kỹ năng", "simple_plugin" ), "activate_plugins", "ds_ky_nang", array( "TT_KyNang", "tt_kynang_page_callback" ) );
+        add_submenu_page( 'tt_teamwork', __( "Thêm mới kỹ năng", "simple_plugin" ), __( "Thêm mới kỹ năng", "simple_plugin" ), "activate_plugins", "new_kynang", array( "TT_KyNang", "tt_new_kynang_callback" ) );
+    
+    
+    }
+    
+    public function tt_load_languages(){
+        load_plugin_textdomain( 'simple_plugin', false, dirname(plugin_basename(__FILE__) ));
+    }
+}
+
+new TT_Teamwork();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
